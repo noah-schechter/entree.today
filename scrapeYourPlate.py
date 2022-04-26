7c893a524ff0207013dffa292c536c998348044a
noah-schechter <noah.schechter1@gmail.com>
2022-04-25 17:22:03 -0700

    Initial commit

---
+++ scrapeYourPlate.py
@@ -0,0 +1,139 @@
+#Selenium INIT
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+
+#Chrome Driver INIT
+s=Service('/Users/noahschechter/Documents/chromedriver') #Fix this to correct location if running in local machine.
+browser = webdriver.Chrome(service=s, options=options)
+url = 'https://rdeapps.stanford.edu/dininghallmenu/'
+#driver = webdriver.Chrome('/Users/noahschechter/documents/chromedriver')
+
+
+#Firestore INIT
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+import datetime
+
+
+#Select location_ID as Location
+def chooseLocation(location_ID):
+    browser.find_element(By.NAME, 'ctl00$MainContent$lstLocations').click()
+    browser.find_element(By.XPATH, '//*[@id="MainContent_lstLocations"]/option[' + str(location_ID) + ']').click()
+
+#Select meal_ID as Meal
+def chooseMeal(meal_ID):
+    browser.find_element(By.NAME, 'ctl00$MainContent$lstMealType').click()
+    browser.find_element(By.XPATH, '//*[@id="MainContent_lstMealType"]/option[' + str(meal_ID) + ']').click()
+
+
+#Processes ingredient text and returns list of high-level ingredients (with lower-level ones inccluded in ingredient strigns)
+def process(ingredients):
+    out = []
+    last = 0
+    search = True
+    i = 0
+    while i < len(ingredients):
+        if search == True and ingredients[i] == ",":
+            out.append(ingredients[last:i])
+            last = i + 2
+        elif ingredients[i] == '(':
+            search = False
+        elif ingredients[i] == ')':
+            search = True
+        else:
+            pass
+        i += 1
+    return out
+        
+
+#scrapes text into dictionary bigIngredientList. Each key is a dish with values as a nested list of ingredients and index.
+def scrape():
+    bigIngredientList = {}
+    dishes = browser.find_elements(By.CSS_SELECTOR, "div.clsMenuItem")
+    i = 0
+    for dish in dishes:
+        name = dish.find_element(By.CSS_SELECTOR, "span.clsLabel_Name")        
+        ingredients = dish.find_element(By.CSS_SELECTOR, "span.clsLabel_Ingredients").text
+        vegetarian = False
+        if "clsV_Row" in dish.get_attribute("class"):
+            vegetarian = True
+        vegan = False
+        if "clsVGN_Row" in dish.get_attribute("class"):
+            vegan = True
+        gluten_free = False
+        if "clsGF_Row" in dish.get_attribute("class"):
+            gluten_free = True
+        start = ingredients.find(':')
+        ingredients = ingredients[start + 2:]
+        ingredients = process(ingredients)
+        if name.text not in bigIngredientList:
+            bigIngredientList[name.text] = [ingredients, i, vegetarian, vegan, gluten_free]
+        i += 1 #We're not getting index==0 foods
+    return bigIngredientList
+
+
+#adds bigIngredientList to Firebase
+def add_new_meal(bigIngredientList, meal_ID):
+    #today = date.today()
+    dateNow = datetime.date.today() 
+    meal = whatMeal(meal_ID)
+    for ingredient in bigIngredientList:
+        db.collection('Foods').add({'Date':str(dateNow), 
+                                    'Meal':meal, 
+                                    'Dish':ingredient, 
+                                    'Ingredients':bigIngredientList[ingredient][0], 
+                                    'Index':bigIngredientList[ingredient][1],
+                                    'Vegetarian':bigIngredientList[ingredient][2],
+                                    'Vegan':bigIngredientList[ingredient][3],
+                                    'Gluten Free':bigIngredientList[ingredient][4]})
+
+
+def whatMeal(meal_ID):
+    if meal_ID == 2:
+        meal = 'breakfast'
+    elif meal_ID== 3:
+        meal = 'lunch'
+    elif meal_ID == 4:
+        meal = 'dinner'
+    elif meal_ID == 5:
+        meal = 'brunch'
+    return meal
+
+"""
+#For eventual inclusion of all dining halls.
+def whatLocation(location_ID):
+    if location_ID == 2:
+       location = 'Arrillaga'
+    elif meal_ID== 3:
+        meal = 'lunch'
+    elif meal_ID == 4:
+        meal = 'dinner'
+    elif meal_ID == 5:
+        meal = 'brunch'
+    return meal
+"""
+
+
+
+if __name__ == "__main__":
+    browser.get(url)
+    for location_ID in range(2,3): #To be improved with all locations, perhaps
+        chooseLocation(location_ID)
+        for meal_ID in range(2,6):
+            chooseMeal(meal_ID)
+            add_new_meal(scrape(), meal_ID)
+    browser.close()
---
+++ website/new.js
@@ -0,0 +1,92 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB_TJfc_ao1rlA4TgzGSnscWgL9_KcMQuM",
+  authDomain: "bettermenu-75f1f.firebaseapp.com",
+  databaseURL: "https://bettermenu-75f1f-default-rtdb.firebaseio.com",
+  projectId: "bettermenu-75f1f",
+  storageBucket: "bettermenu-75f1f.appspot.com",
+  messagingSenderId: "606440182954",
+  appId: "1:606440182954:web:fa55eeb32609e0578eacd3",
+  measurementId: "G-TDTH4NEZKH"
+};
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { where, query, orderBy, limit, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";  
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const dateTime = new Date();
+
+function makeTime() {
+  var year = dateTime.getFullYear();
+  var month = parseInt(dateTime.getMonth()) + 1;
+  if (parseInt(month) < 10) {
+    month = 0 + String(month)
+  }
+  var day = dateTime.getDate();
+  if (parseInt(day) < 10) {
+    day = 0 + String(day)
+  }
+  const date = String(year) + '-' + String(month) + '-' + String(day);
+  return String(date);
+}
+
+
+function whatMeal() {
+  var hours = dateTime.getHours();
+  if (hours < 17) {
+    return "lunch";
+  }
+  return "dinner";
+}
+
+
+var date = makeTime()
+
+
+async function build(meal, course) {
+  var dishes =[]
+  if (course == "entrees") {
+    dishes = await getDocs(query(collection(db, "Foods"), where('Date', '==', `${date}`), where('Meal', '==', `${meal}`), orderBy('Index'), limit(2)));
+  }
+  else if (course == "sides") {
+    dishes = await getDocs(query(collection(db, "Foods"), where('Date', '==', `${date}`), where('Meal', '==', `${meal}`), where('Index', '>=', 2)));
+  }
+  else {
+    dishes = [1]
+  }
+  let contents = document.getElementById(`${course}`); 
+  dishes.forEach(dish => {
+    let food = document.createElement("p");
+    food.innerText = dish.data().Dish.toLowerCase();
+    food.className = "p-4 text-xl";
+    contents.appendChild(food);
+  });
+}
+
+
+function construct(meal) {
+  if (meal == "lunch" && (dateTime.getDay() == 6 || dateTime.getDay() == 0)) {
+      let top = document.getElementById("top");
+      top.innerText = "BRUNCH";
+      let contents = document.getElementById("info");
+      let food = document.createElement("p");
+      food.innerText = "This is supposed to be the lunch part of brunch. Expect breakfast food too. ";
+      contents.appendChild(food);
+    }
+  else {
+    let top = document.getElementById("top");
+    top.innerText = `${meal.toUpperCase()}`;
+  }
+  let entree_header = document.getElementById("entree_header");
+  entree_header.innerText = 'ENTREES';
+  build(meal, "entrees")
+  let sides_header = document.getElementById("sides_header");
+  sides_header.innerText = "SIDES";
+  build(meal, "sides")
+  }
+
+construct(whatMeal());
