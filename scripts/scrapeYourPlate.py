c77e7d39a22de19b1b2bef896f4f37f2d5313c68
Noah Schechter <100018299+noah-schechter@users.noreply.github.com>
2022-09-05 00:51:36 -0700

    Update serviceAccountKey secrets

--- scripts/scrapeYourPlate.py
+++ scripts/scrapeYourPlate.py
@@ -21,7 +21,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate(os.environ['serviceAccountKey.json'])
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
