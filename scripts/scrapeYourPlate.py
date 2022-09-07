2f93684e31cbc6a4ec5cefe2450719c9a1cae701
noah-schechter <noah.schechter1@gmail.com>
2022-09-07 00:11:13 -0700

    Update secrets access

--- scripts/scrapeYourPlate.py
+++ scripts/scrapeYourPlate.py
@@ -21,7 +21,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate(os.environ['serviceAccountKey.json'])
+cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
