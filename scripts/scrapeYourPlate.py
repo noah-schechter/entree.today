9ea002b7bc602e20f667b4315533bd5776034dfe
noah-schechter <noah.schechter1@gmail.com>
2022-09-07 00:13:06 -0700

    Make browser headless

--- scripts/scrapeYourPlate.py
+++ scripts/scrapeYourPlate.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
-#options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
