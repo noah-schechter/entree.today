341266fc8b2a1c2a403119a7036c1058cb316569
noah-schechter <noah.schechter1@gmail.com>
2023-05-23 18:42:12 -0700

    Improve firebase creds

--- scripts/scrapeYourPlate.py
+++ scripts/scrapeYourPlate.py
@@ -24,8 +24,20 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-#cred = credentials.Certificate('serviceAccount')) 
-firebase_admin.initialize_app('serviceAccount')
+
+
+cred = credentials.Certificate({
+    'type': 'service_account',
+    'project_id': os.environ.get('project_id'),
+    'private_key': os.environ.get('serviceAccount'),
+    'client_email': os.environ.get('client_email'),
+    'client_id': os.environ.get('client_id')
+    'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+    'token_uri': 'https://accounts.google.com/o/oauth2/token',
+    'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+    'client_x509_cert_url': os.environ.get('client_x509_cert_url')
+})
+firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
