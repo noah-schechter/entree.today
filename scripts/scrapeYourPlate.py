4605c78a1ab60cce39933e92ac305a47fc7f2963
noah-schechter <noah.schechter1@gmail.com>
2023-05-23 20:15:39 -0700

    Parens fix

--- scripts/scrapeYourPlate.py
+++ scripts/scrapeYourPlate.py
@@ -37,7 +37,7 @@ cred = credentials.Certificate(json.loads({
     "token_uri": "https://accounts.google.com/o/oauth2/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": os.environ.get('client_x509_cert_url')
-})))
+}))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
