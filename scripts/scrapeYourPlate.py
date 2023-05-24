7d478fe9ddf9d37a22fa5f1ae545b9733882e977
noah-schechter <noah.schechter1@gmail.com>
2023-05-23 20:12:26 -0700

    Further JSON optimizations

--- scripts/scrapeYourPlate.py
+++ scripts/scrapeYourPlate.py
@@ -33,9 +33,9 @@ cred = credentials.Certificate(json.loads(str({
     "private_key": os.environ.get('firebaseServiceAccount').replace(r'\n', '\n'),
     "client_email": os.environ.get('client_email'),
     "client_id": os.environ.get('client_id'),
-    "auth_uri": 'https://accounts.google.com/o/oauth2/auth',
-    "token_uri": 'https://accounts.google.com/o/oauth2/token',
-    "auth_provider_x509_cert_url": 'https://www.googleapis.com/oauth2/v1/certs',
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": os.environ.get('client_x509_cert_url')
 })))
 firebase_admin.initialize_app(cred)
